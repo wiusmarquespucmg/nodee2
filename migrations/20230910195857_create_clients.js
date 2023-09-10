61a0c7ffc2c7127064562a6ff5817589abc3ef70
Wiusmarques M dos Reis <1248629@sga.pucminas.br>
2023-09-10 17:30:25 -0300

    feat: routes to manager simple clients

---
+++ migrations/20230910195857_create_clients.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('clients', function(table) {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('phone').notNullable();
+        table.string('cpf');
+        table.string('cep');
+        table.string('address');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('clients');
+};
--- routes/clients.js
+++ routes/clients.js
@@ -1,9 +1,70 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const knex = require('knex');
+const db = knex(require('../knexfile')['development']);
+
+
+// Listando todos os clientes
 router.get('/clients', function(req, res, next) {
-  
+    db.select().from('clients').then((clients) => {
+        res.json(clients);
+    }).catch((err) => {
+        res.status(500).json({ error: err.message });
+    });
+});
+
+
+// Buscando por um cliente específico
+router.get('/clients/:id', function(req, res, next) {
+    db.select().from('clients').where('id', req.params.id).then((clients) => {
+        if (clients.length > 0) {
+            res.json(clients[0]);
+        } else {
+            res.status(404).json({ error: `Cliente com id ${req.params.id} não encontrado`});
+        }
+    }).catch((err) => {
+        res.status(500).json({ error: err.message });
+    });
+});
+
+// Criando um novo cliente
+router.post('/clients', function(req, res, next) {
+    db.insert(req.body).returning('*').into('clients').then((clients) => {
+        res.json(clients[0]);
+    }).catch((err) => {
+        res.status(500).json({ error: err.message });
+    });
+});
+
+// Atualizando um cliente
+router.put('/clients/:id', function(req, res, next) {
+    db('clients').where('id', req.params.id).update(req.body).returning('*').then((clients) => {
+        if (clients.length > 0) {
+            res.json(clients[0]);
+        } else {
+            res.status(404).json({ error: `Cliente com id ${req.params.id} não encontrado`});
+        }
+    }).catch((err) => {
+        res.status(500).json({ error: err.message });
+    });
+});
+
+// Deletando um cliente
+router.delete('/clients/:id', function(req, res, next) {
+    db.select().from('clients').where('id', req.params.id).then((clients) => {
+        if (clients.length <= 0) {
+            res.status(404).json({ error: `Cliente com id ${req.params.id} não encontrado`});
+        }else{
+            db('clients').where('id', req.params.id).del().returning('*').then((clients) => {
+                res.status(200).json({ success: `Cliente com id ${req.params.id} deletado com sucesso` });
+            }).catch((err) => {
+                res.status(500).json({ error: err.message });
+            });
+        }
+    }).catch((err) => {
+        res.status(500).json({ error: err.message });
+    });
 });
 
 module.exports = router;
